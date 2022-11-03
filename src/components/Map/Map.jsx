f80a8a67991056a5d1bf1ff73a0d8362ffa08085
GyanendraMohan <gmpatel152@gmail.com>
2022-11-03 21:17:44 +0530

    implemented Header and Maps

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+import useStyles from './styles'
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coordinates = { lat: 0, lng: 0 };
+
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyCy5_2uSVt0qFVF1mm9dubleLLDxdqQfiA' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                marzin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     )
 }
 
