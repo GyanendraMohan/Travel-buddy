415733e3f61dc76cf8d12077a7e58cf100e88e1d
GyanendraMohan <gmpatel152@gmail.com>
2022-11-04 21:53:32 +0530

    Implemented card to display restaurants

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,22 +3,12 @@ import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, 
 import useStyles from './styles'
 import PlaceDetails from '../PlaceDetails/PlaceDetails'
 
-const List = () => {
+const List = ({ places }) => {
     const classes = useStyles();
     const [type, setType] = useState("restaurants");
     const [rating, setRating] = useState("");
 
-    const places = [
-        { name: 'Cool Place' },
-        { name: "Best Beer" },
-        { name: 'Best Steak' },
-        { name: 'Cool Place' },
-        { name: "Best Beer" },
-        { name: 'Best Steak' },
-        { name: 'Cool Place' },
-        { name: "Best Beer" },
-        { name: 'Best Steak' },
-    ]
+
     return (
         <div className={classes.container}>
             <Typography variant='h4' >
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core'
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles'
 
 const PlaceDetails = ({ place }) => {
+    const classes = useStyles();
     return (
-        <h1>{place.name}</h1>
+        <Card elevation={6}>
+            <CardMedia
+                style={{ height: 350 }}
+                image={place.photo ? place.photo.images.large.url : 'https://images.unsplash.com/photo-1607537669513-8472b5a8f85b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'}
+                title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant='h5'>
+                    {place.name}
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
 
