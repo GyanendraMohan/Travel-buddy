a74e611c971af6853415d7e650e2bdf0082e2756
GyanendraMohan <gmpatel152@gmail.com>
2022-11-04 20:45:26 +0530

    ADDED rapid api and implemented current location

---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "X-RapidAPI-Key": "4d3175e5a2msh4ed09cdae0beaddp1b482bjsn3931e55992aa",
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,10 +5,10 @@ import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles'
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
-    const coordinates = { lat: 0, lng: 0 };
+
 
     return (
         <div className={classes.mapContainer}>
@@ -19,7 +19,10 @@ const Map = () => {
                 defaultZoom={14}
                 marzin={[50, 50, 50, 50]}
                 options={''}
-                onChange={''}
+                onChange={(e) => {
+                    setCoordinates = ({ lat: e.center.lat, lng: e.center.lng });
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+                }}
                 onChildClick={''}
             >
 
